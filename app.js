4ce7560bd8e5e6d8634180b05639304ec730716b
modulati0ns <javimtmz96@gmail.com>
2020-07-16 00:28:53 +0200

    redeficion de funciones

--- app.js
+++ app.js
@@ -3,62 +3,73 @@ const temp = document.querySelector('.temp');
 const celsius = document.querySelector('.celsius');
 const icon = document.querySelector('.icon');
 const api = "e52a4a93bc257d96b056ec72713ac93c";
-const buton = document.querySelector('.but');
-buton.addEventListener("click", cords);
-let endpoint = 'https://api.openweathermap.org/data/2.5/weather?q=' + 'murcia' + '&units=metric' + '&appid=' + api;
 
 
+// App data
+const weather = {};
 
-
-async function getData(url) {
-    const response = await fetch(url);
-
-    return response.json()
+weather.temperature = {
+    unit : "celsius"
 }
 
-var latit, longi;
 
-async function main() {
-    var data = await getData(endpoint);
-    // Aqui tengo los datos en data y los puedo poner en el HTML
-    
-    if (data){
-        latit, longi = await getLocation();
-
-        city.innerHTML = data.name;                             // Edito la ciudad
-        celsius.innerHTML = Math.round(data.main.temp);         // Edito la temperatura
-        icon.innerHTML = '<i class="wi wi-owm-804"></i>'        // Edito el icono
-
-
-    }
-
-
+async function getData(lat, lon) {
+    let endpoint = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=metric&appid=' + api;
+    const response = await fetch(endpoint)
+        .then(function (response) {
+            let data = response.json();
+            return data;
+        })
+        .then(function (data) {
+            weather.temperature.value = Math.floor(data.main.temp);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].id;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        }).then(() => {
+            displayWeather();
+        })
+        .catch((e)=>{
+            console.log('Se ha encontrado un error: '+e);
+        })
 
 }
 
-function getLocation() {
+
+async function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(Position =>
-            {
-                const lat = Position.coords.latitude;
-                const lon = Position.coords.longitude;
-                console.log(lat, lon);
-                return [lat, lon];
-            });
+        navigator.geolocation.getCurrentPosition(Position => {
+            const lat = Position.coords.latitude;
+            const lon = Position.coords.longitude;
+
+            getData(lat, lon)
+
+
+        });
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
-function cords()
-{
-    console.log(latit);
-    console.log(longi);
+function displayWeather() {
+    city.innerHTML = weather.city; // Edito la ciudad
+    celsius.innerHTML = weather.temperature.value; // Edito la temperatura
+    icon.innerHTML = '<i class="wi wi-owm-'+weather.iconId+'"></i>' // Edito el icono
 }
 
 
+// latit, longi = await getLocation().then(()=>{
+//     var dataWeather = getData(endpoint);
+//     // Aqui tengo los datos en data y los puedo poner en el HTML
+
+//     if (dataWeather) {
 
 
-main();
 
-// data => obj = data
+//     }
+// })
+
+
+
+
+getLocation();
